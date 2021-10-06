23b2acf8e23a1fda999471f749df698579f8e6fb
alexRicc2 <alex.65.santanna@gmail.com>
2021-10-06 13:39:55 -0300

    projeto pokedex

---
+++ Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Pokedex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private static List<Pokemon> pokedex = new List<Pokemon>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaPokemon([FromBody] Pokemon pokemon)
+        {
+            pokemon.Id = id++;
+            pokedex.Add(pokemon);
+            return CreatedAtAction(nameof(RetornaPokemonPorId), new { Id = pokemon.Id }, pokemon);
+        }
+
+
+        [HttpGet]
+        public IActionResult RetornaPokedex()
+        {
+            return Ok(pokedex);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RetornaPokemonPorId(int id)
+        {
+            Pokemon pokemon = pokedex.FirstOrDefault(pokemon => pokemon.Id == id);
+            if (pokemon != null) return Ok(pokemon);
+            return NotFound();
+        }
+    }
+}
---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokedex.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        [Required]
+        public string PokemonName { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+
+        public string Type { get; set; }
+
+    }
+}
